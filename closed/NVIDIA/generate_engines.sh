58618826ca1e8673bac749290e8a4d4e5755cf5d
gunjunlee <gunjunlee97@gmail.com>
2024-06-11 02:22:29 +0900

    minor fix

--- closed/NVIDIA/generate_engines.sh
+++ closed/NVIDIA/generate_engines.sh
@@ -19,11 +19,16 @@ for CVD in "${CVDS[@]}"; do
     for scenario in Offline Server; do
         for gpu_batch_size in 1 2 4 8; do
             echo "build scenario=${scenario} bs=${gpu_batch_size} gpus=${CVD}"
+            LOGDIR="build/logs/generate_engines/scenario-{}-bs-{}-gpus-{}"
+            mkdir -p $LOGDIR
+            LOG_OUT=${LOGDIR}/stdout.log
+            LOG_ERR=${LOGDIR}/stderr.log
             LD_LIBRARY_PATH=$CONDA_PREFIX/lib
                 CPATH=$CONDA_PREFIX/include \
                 CXXPATH=$CONDA_PREFIX/include \
                 MLPERF_SCRATCH_PATH=$PWD CUDA_VISIBLE_DEVICES=${CVD} \
-                make generate_engines RUN_ARGS="--benchmarks=stable-diffusion-xl --scenarios=Server --gpu_batch_size=${gpu_batch_size}" \
+                make generate_engines RUN_ARGS="--benchmarks=stable-diffusion-xl --scenarios=${scenario} --gpu_batch_size=${gpu_batch_size}" \
+                > $LOG_OUT 2> $LOG_ERR \
                 && echo "Generating engine done" || echo "Generating engine failed"
             done;
     done;
