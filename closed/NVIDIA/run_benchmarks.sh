5162add1c2ee9a04b5beb3842be233f1594e04c7
gunjunlee <gunjunlee97@gmail.com>
2024-06-12 17:19:00 +0900

    fix Offline qps

--- closed/NVIDIA/run_benchmarks.sh
+++ closed/NVIDIA/run_benchmarks.sh
@@ -16,7 +16,7 @@ fi
 echo "CUDA_VISIBLE_DEVICES = ${CVDS[*]}"
 
 for CVD in "${CVDS[@]}"; do
-    for scenario in Server Offline; do
+    for scenario in Offline Server; do
         for gpu_batch_size in 1 2 4 8; do
             for qps in 0.2 0.3 0.5 0.7 0.9 1.1 1.3 1.5; do
                 echo "run scenario=${scenario} bs=${gpu_batch_size} gpus=${CVD} qps=${qps}"
@@ -30,7 +30,7 @@ for CVD in "${CVDS[@]}"; do
                 LD_LIBRARY_PATH=$CONDA_PREFIX/lib \
                     MLPERF_SCRATCH_PATH=$PWD \
                     CUDA_VISIBLE_DEVICES=${CVD} \
-                    make run_harness RUN_ARGS="--benchmarks=stable-diffusion-xl --scenarios=${scenario} --test_mode=PerformanceOnly --fast --log_dir=${LOGDIR} --gpu_batch_size=${gpu_batch_size} --server_target_qps=${qps}" \
+                    make run_harness RUN_ARGS="--benchmarks=stable-diffusion-xl --scenarios=${scenario} --test_mode=PerformanceOnly --fast --log_dir=${LOGDIR} --gpu_batch_size=${gpu_batch_size} --server_target_qps=${qps} --offline_expected_qps=${qps}" \
                     > $LOG_OUT 2> $LOG_ERR \
                     && echo "Running benchmark done" || echo "Running benchmark failed"
             done;
